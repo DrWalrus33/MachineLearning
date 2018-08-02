91b627fcfec423aa358add692f7ef49ef1228eb3
iD Student <coopermurr33@gmail.com>
2018-08-02 13:55:25 -0700

    hi2

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import numpy as np
 
 class ConvNet:
     def __init__(self, image_height, image_width, channels, num_classes):
+        print("test1")
         self.input_layer = tf.placeholder(dtype=tf.float32, shape = [None, image_height, image_width, channels])
         print (self.input_layer.shape)
 
@@ -19,13 +20,13 @@ class ConvNet:
 
         #Second layer defining
 
-        conv_layer_2 = tf.layers.conv2d(pooling_layer_1, filters=64, kernel_size=[5, 5], padding="same",activation=tf.nn.relu)
+        """conv_layer_2 = tf.layers.conv2d(pooling_layer_1, filters=64, kernel_size=[5, 5], padding="same",activation=tf.nn.relu)
         print(conv_layer_2.shape)
 
         pooling_layer_2 = tf.layers.max_pooling2d(conv_layer_2, pool_size=[2, 2], strides=2)
-        print(pooling_layer_2.shape)
+        print(pooling_layer_2.shape)"""
 
-        flatten_pooling = tf.layers.flatten(pooling_layer_2)
+        flatten_pooling = tf.layers.flatten(pooling_layer_1)
 #       Adding more neurons
 
         dense_layer = tf.layer.dense(flatten_pooling, 1024, activation=tf.nn.relu)
@@ -55,7 +56,7 @@ image_height = 32
 
 image_width = 32
 
-training_steps = 20000
+training_steps = 5000
 
 batch_size = 64
 
@@ -67,7 +68,7 @@ load_checkpoint = False
 
 color_channels = 3
 
-
+print("test2")
 
 
 def unpickle(file):
@@ -76,7 +77,7 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-
+print("test3")
 cifar_path = '/home/student/Desktop/MachineLearningmUrr/cifar-10-data/'
 
 train_data = np.array([])
@@ -84,13 +85,14 @@ train_labels = np.array([])
 
 # Load all the data batches.
 for i in range(1, 6):
+    print("test4")
     data_batch = unpickle(cifar_path + 'data_batch_' + str(i))
     train_data = np.append(train_data, data_batch[b'data'])
     train_labels = np.append(train_labels, data_batch[b'labels'])
 
 # Load the eval batch.
 eval_batch = unpickle(cifar_path + 'test_batch')
-
+print("test5")
 eval_data = eval_batch[b'data']
 eval_labels = eval_batch[b'labels']
 
@@ -101,25 +103,31 @@ category_names = list(map(lambda x: x.decode("utf-8"), category_names_bytes))
 
 # TODO: Process Cifar data
 
-
+print("test6")
 
 def process_data(data):
     float_data = np.array(data, dtype=float) / 255.0
-
+    print("test7")
     reshaped_data = np.reshape(float_data, (-1, image_height, image_width, color_channels))
     transposed_data = np.transpose(reshaped_data, [0,2,3,1])
     return transposed_data
 
 train_data = process_data(train_data)
 eval_data = process_data(eval_data)
-
+print("test8")
 tf.reset_default_graph()
-
+print("test9")
 dataset= tf.data.Dataset.from_tensor_slices((train_data, train_labels))
+print("testa")
 dataset = dataset.shuffle(buffer_size=train_labels.shape[0])
+print("testb")
 dataset = dataset.batch(batch_size)
+print("testc")
 dataset = dataset.repeat()
+print("testd")
+print("testtt")
 dataset_iterator = dataset.make_initializable_iterator()
+print("test10")
 next_element = dataset_iterator.get_next()
-
+print("test11")
 cnn = ConvNet(image_height, image_width, color_channels, 10)
